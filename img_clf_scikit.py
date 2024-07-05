a34ee66f01de23cf194872f46f35fcce14fb4b91
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-07-06 01:00:53 +0600

    work in progress

--- img_clf_scikit.py
+++ img_clf_scikit.py
@@ -4,9 +4,11 @@ import os
 from skimage.io import imread
 from skimage.transform import resize
 import numpy as np
+from sklearn.model_selection import train_test_split
+
 # prepare data
 
-input_directory = 'C:/Users/Baishakhi/PycharmProjects/myPython/opencv_python/imageClassification/data'
+input_directory = 'C:/Users/Baishakhi/PycharmProjects/myPython/imageClassification/data'
 categories = ['empty', 'not_empty']
 
 data = []
@@ -23,4 +25,5 @@ data = np.asarray(data)
 label = np.asarray(label)
 
 # train/ test split
+a_train, a_test, y_train, y_test = train_test_split(data, label, test_size = 0.3, shuffle= True, stratify= label)
 # test performance
