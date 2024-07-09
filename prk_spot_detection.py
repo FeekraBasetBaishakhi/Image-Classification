bfbc2b3838d1dc1fbc2660ca6a6962cb8d20d40f
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-07-10 01:29:57 +0600

    park spot counter -work in progress

--- prk_spot_detection.py
+++ prk_spot_detection.py
@@ -1,1 +1,35 @@
-import cv
+# feekra baset
+import cv2
+
+from util import get_parking_spot_boxes
+
+
+mask_path = './mask.png'
+
+video_path = './data/parkinglot.mp4'
+
+
+mask = cv2.imread(mask_path, 0)
+
+capture = cv2.VideoCapture(video_path)
+
+connected_compnts = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+
+spots = get_parking_spot_boxes(connected_compnts)
+ret = True
+
+while ret:
+    ret, frame = capture.read()
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(30) & 0xFF == ord('y'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
