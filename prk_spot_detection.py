0e891525026560f1155247d8e08e127aefeb5719
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-07-15 21:36:13 +0600

    binary mask corrected

--- prk_spot_detection.py
+++ prk_spot_detection.py
@@ -6,20 +6,24 @@ from util import get_parking_spot_boxes
 
 mask_path = './mask.png'
 
-video_path = './data/parkinglot.mp4'
+video_path = './data/park.mp4'
 
 
 mask = cv2.imread(mask_path, 0)
 
 capture = cv2.VideoCapture(video_path)
 
-connected_compnts = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+connected_compnts = cv2.connectedComponentsWithStats(mask, 3, cv2.CV_32S)
 
 spots = get_parking_spot_boxes(connected_compnts)
 ret = True
 
 while ret:
     ret, frame = capture.read()
+    for spot in spots:
+        a3, b3, w, h = spot
+
+        frame = cv2.rectangle(frame,(a3,b3), (a3 + w, b3 + h), (0, 0, 255), 4)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(30) & 0xFF == ord('y'):
--- util.py
+++ util.py
@@ -7,7 +7,7 @@ import cv2
 zero = True
 one = False
 
-model = pickle.load(open("model.p", "rb"))
+MODEL = pickle.load(open("model.p", "rb"))
 
 
 def zero_or_not(spot_bgr):
@@ -17,7 +17,7 @@ def zero_or_not(spot_bgr):
     flat_data.append(image_resize.flatten())
     flat_data = np.array(flat_data)
 
-    y_output = model.predict(flat_data)
+    y_output = MODEL.predict(flat_data)
 
     if y_output == 0:
         return zero
@@ -26,17 +26,17 @@ def zero_or_not(spot_bgr):
 
 
 def get_parking_spot_boxes(connected_compnts):
-    (totaLabels, label_ids, values, centric) = connected_compnts
+    (totalLabels, label_ids, values, centric) = connected_compnts
 
     slot = []
-    coof = 1
+    coef = 1
 
-    for i in range(1, totaLabels):
+    for i in range(1, totalLabels):
         # coordinate points
-        a3 = int(values[i, cv2.CC_STAT_LEFT] * coof)
-        b3 = int(values[i, cv2.CC_STAT_TOP] * coof)
-        w = int(values[i, cv2.CC_STAT_WIDTH] * coof)
-        h = int(values[i, cv2.CC_STAT_HEIGHT] * coof)
+        a3 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+        b3 = int(values[i, cv2.CC_STAT_TOP] * coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
 
         slot.append([a3, b3, w, h])
 
