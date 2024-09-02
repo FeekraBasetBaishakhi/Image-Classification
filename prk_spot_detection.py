025b59b1f7b66a724a80187a3db1cefc2be05b80
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-09-03 01:57:14 +0600

    correction

--- prk_spot_detection.py
+++ prk_spot_detection.py
@@ -1,6 +1,8 @@
 # feekra baset
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+
 from util import get_parking_spot_boxes, zero_or_not
 
 
@@ -8,11 +10,11 @@ def calc_diff(im1, im2):  # Compute the value how different the two spots are
     return np.abs(np.mean(im1) - np.mean(im2))  # for absolute number shortform abs
 
 
-mask_path = './mask_crop.png'
+mask = './mask.jpg'
 
-video_path = './data/parking_crop.mp4'
+video_path = './data/parking_1920_1080_loop.mp4'
 
-mask = cv2.imread(mask_path, 0)
+mask = cv2.imread(mask, 0)
 
 capture = cv2.VideoCapture(video_path)
 
@@ -40,9 +42,16 @@ while ret:
 
             diffs[spot_indx] = calc_diff(spot_crp, previous_frame[b3:b3 + h, a3:a3 + w, :])
 
-        print(diffs)
+        print([diffs[j] for j in np.argsort(diffs)][::-1])
+
     if frame_nmr % step == 0:
-        for spot_indx, spot in enumerate(spots):  # calling the rectangle from using binary mask
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs) > 0.4][
+                   ::-1]  # calling the rectangle from using binary mask
+        for spot_indx in arr_:
+            spot = spots[spot_indx]
             a3, b3, w, h = spot
 
             spot_crp = frame[b3:b3 + h, a3:a3 + w, :]
@@ -59,13 +68,17 @@ while ret:
 
             a3, b3, w, h = spots[spot_indx]
 
-    if spot_status:
-        frame = cv2.rectangle(frame, (a3, b3), (a3 + w, b3 + h), (0, 255, 0), 2)
-    else:
-        frame = cv2.rectangle(frame, (a3, b3), (a3 + w, b3 + h), (0, 0, 255), 2)
+            if spot_status:
+                frame = cv2.rectangle(frame, (a3, b3), (a3 + w, b3 + h), (0, 255, 0), 2)
+            else:
+                frame = cv2.rectangle(frame, (a3, b3), (a3 + w, b3 + h), (0, 0, 255), 2)
 
+    cv2.rectangle(frame, (80, 20), (550, 80), (0, 0, 0), -1)
+    cv2.putText(frame, 'Available spots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
-    if cv2.waitKey(30) & 0xFF == ord('y'):
+    if cv2.waitKey(25) & 0xFF == ord('y'):
         break
 
     frame_nmr += 1
