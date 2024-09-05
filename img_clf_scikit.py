500ae5da8ccff10bbedeee2af541fa86aba17e57
Feekra Baset Baishakhi <65058830+FeekraBasetBaishakhi@users.noreply.github.com>
2024-09-06 00:21:24 +0600

    Image classification scikit learn

--- img_clf_scikit.py
+++ img_clf_scikit.py
@@ -1,14 +1,16 @@
-# feekra baset
+#  feekra baset
 import os
-
+import pickle
 from skimage.io import imread
 from skimage.transform import resize
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+# preparing the data
 
-# prepare data
-
-input_directory = 'C:/Users/Baishakhi/PycharmProjects/myPython/imageClassification/data'
+input_directory = '/data/clf-data'
 categories = ['empty', 'not_empty']
 
 data = []
@@ -17,13 +19,33 @@ for category_idx, category in enumerate(categories):
     for file in os.listdir(os.path.join(input_directory, category)):
         image_path = os.path.join(input_directory, category, file)
         image = imread(image_path)
-        image = resize(image, (15, 16))
-        data.append(image.flatten())
+        image = resize(image, (15, 15))
+        data.append(image.flatten())  # to make an array
         label.append(category_idx)
 
 data = np.asarray(data)
 label = np.asarray(label)
 
-# train/ test split
-a_train, a_test, y_train, y_test = train_test_split(data, label, test_size = 0.3, shuffle= True, stratify= label)
-# test performance
+# training / test spliting
+x_train, x_test, y_train, y_test = train_test_split(data, label, test_size=0.3, shuffle=True, stratify=label)
+
+# training classifier
+classifier = SVC()
+
+parameters = [{'gamma': [0.01, 0.001, 0.001], 'C':[1, 10, 100, 1000]}]
+
+grid_search = GridSearchCV(classifier,parameters)
+
+grid_search.fit(x_train, y_train)
+
+# testing performance
+best_estimator = grid_search.best_estimator_
+
+y_prediction = best_estimator.predict(x_test)
+
+score = accuracy_score(y_prediction, y_test)
+
+print('{}% of Samples Arere Correctly Classified'.format(str(score * 100)))
+
+# pickle.dump(best_estimator, open('./model.p', 'wb'))
+
